a8cecf72efd2f7e5a2dc5810e5299f73e8d42b5b
Michael Klishin <michael@rabbitmq.com>
2015-03-30 01:45:15 +0300

    Introduce Lapin.Exchange

---
+++ RabbitMQ.Lapin.Tests/ExchangeTests.fs
@@ -0,0 +1,67 @@
+﻿namespace Lapin
+
+open System
+
+open NUnit.Framework
+open FsUnit
+
+open Lapin.Core
+open Lapin.Channel
+open Lapin.Queue
+open Lapin.Exchange
+open RabbitMQ.Client
+
+module ExchangeTests =
+
+    [<TestFixture>]
+    type ExchangeTests () =
+        let defaultFanout = "lapin.tests.fanout"
+
+        member t.DeclareArgs = {name = defaultFanout;
+                                ``type`` = Fanout;
+                                durable = false;
+                                autoDelete = false;
+                                arguments = None}
+
+        [<Test>]
+        member t.``exchange.declare with fanout type and default attributes``() =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch   = Lapin.Channel.``open``(conn)
+            Lapin.Exchange.delete(ch, defaultFanout, None)
+            Lapin.Exchange.assertExistence(conn, defaultFanout) |> should equal false
+            Lapin.Exchange.declare(ch, t.DeclareArgs)
+            Lapin.Exchange.assertExistence(conn, defaultFanout) |> should equal true
+
+        [<Test>]
+        member t.``assertExistence when exchange exists`` () =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch   = Lapin.Channel.``open``(conn)
+            let s    = "lapin.tests.topic"
+            Lapin.Exchange.declare(ch, {t.DeclareArgs with name = s; ``type`` = Topic}) |> ignore
+            Lapin.Exchange.assertExistence(conn, s) |> should equal true
+            Lapin.Exchange.delete(ch, s, None)
+
+        [<Test>]
+        member t.``assertExistence when queue DOES NOT exist`` () =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch   = Lapin.Channel.``open``(conn)
+            let s    = Guid.NewGuid().ToString()
+            Lapin.Exchange.delete(ch, s, None)
+            Lapin.Exchange.assertExistence(conn, s) |> should equal false
+
+        [<Test>]
+        member t.``deleteExchange when queue DOES NOT exist is a no-op`` () =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch   = Lapin.Channel.``open``(conn)
+            let s    = Guid.NewGuid().ToString()
+            Lapin.Exchange.delete(ch, s, None)
+
+        [<Test>]
+        member t.``deleteExchange when queue exists`` () =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch   = Lapin.Channel.``open``(conn)
+            let s    = "lapin.tests.topic"
+            Lapin.Exchange.declare(ch, {t.DeclareArgs with name = s; ``type`` = Topic}) |> ignore
+            Lapin.Exchange.assertExistence(conn, s) |> should equal true
+            Lapin.Exchange.delete(ch, s, None)
+            Lapin.Exchange.assertExistence(conn, s) |> should equal false
---
+++ RabbitMQ.Lapin/Exchange.fs
@@ -0,0 +1,51 @@
+﻿namespace Lapin
+
+open RabbitMQ.Client
+open RabbitMQ.Client.Exceptions
+open Lapin.Core
+open Lapin.Types
+open Lapin.Channel
+
+open System
+
+module Exchange =
+    type ExchangeType = Fanout | Topic | Direct | Headers | ConsistentHash | Custom of string
+
+    type ExchangeOptions = {
+        name: Name
+        ``type``: ExchangeType
+        durable: Durable
+        autoDelete: AutoDelete
+        arguments: Arguments
+    }
+
+    let private stringifyExchangeType(input: ExchangeType) =
+        match input with
+        | Fanout   -> "fanout"
+        | Topic    -> "topic"
+        | Direct   -> "direct"
+        | Headers  -> "headers"
+        | ConsistentHash -> "x-consistent-hash"
+        | Custom s -> s
+
+    let declare(ch: IChannel, opts: ExchangeOptions): unit =
+        let args = Lapin.Types.argumentsToIDictionary(opts.arguments)
+        let et   = stringifyExchangeType(opts.``type``)
+        ch.ExchangeDeclare(opts.name, et, opts.durable, opts.autoDelete, args)
+
+    let assertExistence(conn: IConnection, s: Name) =
+        try
+            let tmpCh = Lapin.Channel.``open``(conn)
+            tmpCh.ExchangeDeclarePassive(s) |> ignore
+            true
+        with
+            | :? OperationInterruptedException -> false
+
+    type DeleteOptions = {
+        ifUnused: bool
+    }
+
+    let delete(ch: IChannel, exchange: Name, opts: Option<DeleteOptions>): unit =
+        match opts with
+        | Some del -> ch.ExchangeDelete(exchange, del.ifUnused) |> ignore
+        | None     -> ch.ExchangeDelete(exchange)
