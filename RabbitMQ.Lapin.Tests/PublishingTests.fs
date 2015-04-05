f8cfd10b03c81e021a3377372cf7b6c5420c7611
Michael Klishin <michael@rabbitmq.com>
2015-04-05 18:33:09 +0300

    Introduce basic.get functions
    Fixes #3.

--- RabbitMQ.Lapin.Tests/PublishingTests.fs
+++ RabbitMQ.Lapin.Tests/PublishingTests.fs
@@ -14,6 +14,7 @@ module PublishingTests =
     type ExchangeTests() =
         let defaultFanout = "lapin.tests.fanout"
         let enc = Encoding.UTF8
+        let defaultTimespan = TimeSpan.FromSeconds(5.0)
 
         member t.ExchangeDeclareArgs =
             { name = defaultFanout
@@ -53,4 +54,32 @@ module PublishingTests =
                                 Some { mandatory = true
                                        properties = None })
             Lapin.Queue.messageCount(ch, q) |> should equal 1
+            Lapin.Queue.delete(ch, q, None)
+
+        [<Test>]
+        member t.``basic.publish followed by basic.get with automatic ack``() =
+            use conn = Lapin.Core.connectWithAllDefaults()
+            let ch = Lapin.Channel.``open``(conn) |> enablePublisherConfirms
+            let q = Guid.NewGuid().ToString()
+            Lapin.Exchange.declare(ch, t.ExchangeDeclareArgs)
+            Lapin.Queue.declare(ch, { name = q
+                                      durable = false
+                                      exclusive = false
+                                      autoDelete = false
+                                      arguments = None })
+            |> ignore
+            Lapin.Queue.bind(ch, { exchange = defaultFanout
+                                   queue = q
+                                   routingKey = ""
+                                   arguments = None })
+            let body = enc.GetBytes("msg")
+            Lapin.Basic.publish(ch, { exchange = defaultFanout
+                                      routingKey = "" }, body,
+                                Some { mandatory = true
+                                       properties = None })
+            Lapin.Channel.waitForConfirms(ch, defaultTimespan) |> should equal true
+            let resp = Lapin.Basic.getAutoAck(ch, q)
+            resp.body |> should equal body
+            resp.routingContext.routingKey |> should equal ""
+            resp.redelivered |> should equal false
             Lapin.Queue.delete(ch, q, None)
